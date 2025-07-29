a3c66b46db10335af38657306e18585f749eecdc
lutzweber <37860192+lutzweber@users.noreply.github.com>
2025-07-29 13:40:05 +0200

    Add files via upload
    new version without CDK

--- src/main/java/com/molgenie/assignment/SmilesLoader.java
+++ src/main/java/com/molgenie/assignment/SmilesLoader.java
@@ -35,20 +35,6 @@ import org.openscience.cdk.smiles.SmiFlavor;
 import org.openscience.cdk.smiles.SmilesGenerator;
 import org.openscience.cdk.smiles.SmilesParser;
 
-/**
- * Read smiles and ocid from input file.
- * 
- * <h3>Changelog</h3>
- * <ul>
- *   <li>2022-05-15
- *     <ul>
- *       <li>third version</li>
- *     </ul>
- *   </li>
- * </ul>
- * 
- * @author lutz.weber@ontochem.com
- */
 public class SmilesLoader {
 	
 	private final static Pattern PAT_TAB_SPLIT = Pattern.compile( "\t" );
@@ -62,16 +48,12 @@ public class SmilesLoader {
 	*                   OCID/ID in 2nd column
 	* 
 	* @return  map with OCID as key, SMILES as value
-	 * @throws Exception 
+	* @throws Exception 
 	*/
 	public static Map<String,String> readSmiles( String _fileName, int _max, boolean _verbose ) throws Exception {
 		
 		LOG.info( "reading compounds: " + _fileName );
 		final Map<String,String> targetMap = new HashMap<>();
-		
-		SmilesGenerator _smilesG = getCdkSmilesGenerator();
-		SmilesParser _smilesP = getCdkSmilesParser();
-		Aromaticity _arom = loadAromatizationModule();
 		
 		try ( BufferedReader inCsv = new BufferedReader( 
 		                               new InputStreamReader(
@@ -87,12 +69,9 @@ public class SmilesLoader {
 				String[] splitLine = inLine.split( "," );
 				if ( splitLine.length < 2 ) continue;
 				String smiles = splitLine[0];
-				String ocid   = splitLine[1];
+				String id     = splitLine[1];
 				try {
-					//String normSmiles = normalizeSmilesCDK( smiles, _smilesG, _smilesP, _arom );
-					//if ( _verbose ) System.out.println ( normSmiles );
-					//targetMap.put( ocid, normSmiles  );
-					targetMap.put( ocid, smiles  );
+					targetMap.put( id, smiles  );
 				} catch (Exception e) {
 					// TODO: handle exception
 				}
@@ -102,45 +81,6 @@ public class SmilesLoader {
 			LOG.info( "ERROR: " + er.getLocalizedMessage() );
 		}
 		return targetMap;
-		
 	}
-	
-	public static String normalizeSmilesCDK( String _smiles, SmilesGenerator _smilesG, 
-												SmilesParser _smilesP, Aromaticity _arom ) throws Exception {
-		
-		IAtomContainer mol = _smilesP.parseSmiles( _smiles );
-		_arom.apply( mol );
-		org.openscience.cdk.tools.manipulator.AtomContainerManipulator.convertImplicitToExplicitHydrogens( mol ); 
-		String stdSmiles = _smilesG.create( mol );
-		
-        return stdSmiles;
-    }
-	
-	public static org.openscience.cdk.smiles.SmilesGenerator getCdkSmilesGenerator() {
-		org.openscience.cdk.smiles.SmilesGenerator smilesG = 
-			new org.openscience.cdk.smiles.SmilesGenerator( SmiFlavor.Isomeric );
-		return smilesG;
-	}
-	
-	public static org.openscience.cdk.smiles.SmilesParser getCdkSmilesParser() {
-		org.openscience.cdk.smiles.SmilesParser smilesP = 
-			new org.openscience.cdk.smiles.SmilesParser( SilentChemObjectBuilder.getInstance() );
-		smilesP.kekulise( true );
-		return smilesP;
-	}
-	
-	public static Aromaticity loadAromatizationModule() {
-		try {
-			ElectronDonation model = org.openscience.cdk.aromaticity.ElectronDonation.daylight();
-			//ElectronDonation model = org.openscience.cdk.aromaticity.ElectronDonation.cdk();
-			CycleFinder cycles = Cycles.all();
-			Aromaticity arom = new org.openscience.cdk.aromaticity.Aromaticity( model, cycles );
-			return arom;
-		} catch (Exception e) {
-			LOG.info( "ERROR: load Aromatization Module Error " + e );
-			return null;
-		}
-	}
-
 }
 
